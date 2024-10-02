13729794e530f7e67a7f37a317bd0a2e09aab8f4
Kreskowl <kreskow1@gmail.com>
2024-10-02 08:23:40 +0300

    refactor history methods, add feature saving history in map, refactor tests and task manager methods due to the feature

--- src/ru/yandex/practicum/history/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/history/InMemoryHistoryManager.java
@@ -1,38 +1,70 @@
 package ru.yandex.practicum.history;
 
-import ru.yandex.practicum.manager.Managers;
 import ru.yandex.practicum.task.Task;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
+    private Node<Task> head;
+    private Node<Task> tail;
+    private final HashMap<Integer, Node<Task>> history = new HashMap<>();
 
-    private final LinkedList<Task> history = new LinkedList<>();
-    private final static int MAX_SIZE = 10;
 
     @Override
     public void add(Task task) {
-        if (isNotNull(task)) {
-            if (isFull()) {
-                history.removeFirst();
-                history.add(history.size(), task);
-            } else {
-                history.add(task);
-            }
-        }
+        remove(task.getId());
+        linkLast(task);
     }
 
     @Override
     public List<Task> getHistory() {
-        return new LinkedList<>(history);
+        return getTasks();
     }
 
-    private boolean isFull() {
-        return history.size() >= MAX_SIZE;
+    @Override
+    public void remove(int id) {
+        Node<Task> node = history.remove(id);
+        if (node != null) {
+            removeNode(node);
+        }
     }
 
-    private boolean isNotNull(Task task) {
-        return task != null;
+    private void linkLast(Task task) {
+        Node<Task> node = new Node<>(tail, task, null);
+        if (tail != null){
+            tail.next = node;
+        }
+        tail = node;
+
+        if (head == null){
+            head = node;
+        }
+        history.put(task.getId(), node);
+    }
+
+   private List<Task> getTasks(){
+        List<Task> result = new ArrayList<>();
+        Node<Task> actual = head;
+        while (actual != null) {
+            result.add(actual.data);
+            actual = actual.next;
+        }
+        return result;
+    }
+
+    private void removeNode(Node<Task> node) {
+        if (node.prev == null) {
+            head = node.next;
+        } else {
+            node.prev.next = node.next;
+        }
+        if (node.next == null) {
+            tail = node.prev;
+        } else {
+            node.next.prev = node.prev;
+        }
     }
 }
